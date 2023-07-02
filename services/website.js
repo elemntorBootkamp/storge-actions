93057817d8cb3896ce98d8e310df26b5715c2e6e
rivkapash <pashrivka@gmail.com>
2023-07-02 17:28:36 +0300

    startstop

--- services/website.js
+++ services/website.js
@@ -2,25 +2,20 @@ import logger from '../logger.js';
 import Website from '../models/website.js';
 
 const startStopWebsite = async (websiteId) => {
-try
-{
-   const website = await Website.findById(websiteId);
-   if (website.status === 'About to be inactive')
-   {
-      console.log('enter to if')
-       website.status = 'Inactive';
-       await website.save();
-       logger.info(`seccuss change status to ${ website.status}`);
-   } else if (website.status === 'About to be active')
-{
-   website.status = 'Active';
-   await website.save();
-   logger.info(`seccuss change status to ${ website.status}`);
-}
-else logger.error(`This website is already ${ website.status}`);
- } catch (error)
-{
-   logger.error('Internal Server Error');
-}
+  try {
+    const website = await Website.findById(websiteId);
+    if (website.status === 'About to be inactive') {
+      console.log('enter to if');
+      website.status = 'Inactive';
+      await website.save();
+      logger.info(`seccuss change status to ${website.status}`);
+    } else if (website.status === 'About to be active') {
+      website.status = 'Active';
+      await website.save();
+      logger.info(`seccuss change status to ${website.status}`);
+    } else logger.error(`This website is already ${website.status}`);
+  } catch (error) {
+    logger.error('Internal Server Error');
+  }
 };
 export default startStopWebsite;
