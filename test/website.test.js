c946309afd9133151335dc6ce5a9b085b11050dd
cheina1588 <cheina1588@gmail.com>
2023-07-13 12:20:07 +0300

    testToStartStopPart1

--- test/website.test.js
+++ test/website.test.js
@@ -1,5 +1,6 @@
 import {
-  getAll, getWebById, deleteWebsite, goingDeleteWebsite, startStopWebsitePart2,
+  getAll, getWebById, deleteWebsite, goingDeleteWebsite,
+  startStopWebsitePart1, startStopWebsitePart2,
 } from '../services/website.js';
 import Website from '../models/website.js';
 
@@ -166,67 +167,90 @@ describe('deleteWebsite', () => {
     expect(result).toEqual({ error: error.message });
   });
 });
-describe('startStopWebsitePart2', () => {
+describe('startStopWebsitePart1', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should return an error message if the website is not found', async () => {
-    const websiteId = '6476fb4b3eff4848430b4f93';
-    Website.findById.mockResolvedValue(null);
+  it('should change the status to "About to be inactive" if the website status is "Active"', async () => {
+    const websiteId = '123';
+    const website = {
+      id: '123',
+      status: 'Active',
+      save: jest.fn(),
+    };
+    Website.findById = jest.fn().mockResolvedValue(website);
 
-    const result = await startStopWebsitePart2(websiteId);
-
-    expect(Website.findById).toHaveBeenCalledWith(websiteId);
-    expect(result).toEqual({ error: 'Website doesnt found' });
-  });
-
-  it('should change the status to "Inactive" if the current status is "About to be inactive"', async () => {
-    const websiteId = '6476fb4b3eff4848430b4f93';
-    const website = { status: 'About to be inactive', save: jest.fn() };
-    Website.findById.mockResolvedValue(website);
-
-    const result = await startStopWebsitePart2(websiteId);
+    const result = await startStopWebsitePart1(websiteId);
 
     expect(Website.findById).toHaveBeenCalledWith(websiteId);
-    expect(website.status).toBe('Inactive');
+    expect(website.status).toBe('About to be inactive');
     expect(website.save).toHaveBeenCalled();
-    expect(result).toEqual({ success: true, message: `seccuss change status to ${website.status}` });
+    expect(result).toEqual({
+      success: true,
+      message: 'seccuss change status to About to be inactive',
+    });
   });
 
-  it('should change the status to "Active" if the current status is "About to be active"', async () => {
-    const websiteId = '6476fb4b3eff4848430b4f93';
-    const website = { status: 'About to be active', save: jest.fn() };
-    Website.findById.mockResolvedValue(website);
+  it('should change the status to "About to be active" if the website status is "Inactive"', async () => {
+    const websiteId = '123';
+    const website = {
+      id: '123',
+      status: 'Inactive',
+      save: jest.fn(),
+    };
+    Website.findById = jest.fn().mockResolvedValue(website);
 
-    const result = await startStopWebsitePart2(websiteId);
+    const result = await startStopWebsitePart1(websiteId);
 
     expect(Website.findById).toHaveBeenCalledWith(websiteId);
-    expect(website.status).toBe('Active');
+    expect(website.status).toBe('About to be active');
     expect(website.save).toHaveBeenCalled();
-    expect(result).toEqual({ success: true, message: `seccuss change status to ${website.status}` });
+    expect(result).toEqual({
+      success: true,
+      message: 'seccuss change status to About to be active',
+    });
   });
 
-  it('should return an error message if the current status is neither "About to be inactive" nor "About to be active"', async () => {
-    const websiteId = '6476fb4b3eff4848430b4f93';
-    const website = { status: 'Active' };
-    Website.findById.mockResolvedValue(website);
+  it('should return an error if the website is already in the desired status', async () => {
+    const websiteId = '123';
+    const website = {
+      id: '123',
+      status: 'About to be inactive',
+    };
+    Website.findById = jest.fn().mockResolvedValue(website);
 
-    const result = await startStopWebsitePart2(websiteId);
+    const result = await startStopWebsitePart1(websiteId);
 
     expect(Website.findById).toHaveBeenCalledWith(websiteId);
-    expect(result).toEqual({ error: `This website is already ${website.status}` });
+    expect(result).toEqual({
+      error: 'This website is already About to be inactive',
+    });
   });
 
-  it('should return an error message if an internal error occurs during the process', async () => {
-    const websiteId = '6476fb4b3eff4848430b4f93';
-    const error = new Error('Internal error');
-    Website.findById.mockRejectedValue(error);
+  it('should return an error if the website is not found', async () => {
+    const websiteId = '123';
+    Website.findById = jest.fn().mockResolvedValue(null);
 
-    const result = await startStopWebsitePart2(websiteId);
+    const result = await startStopWebsitePart1(websiteId);
 
     expect(Website.findById).toHaveBeenCalledWith(websiteId);
-    expect(result).toEqual({ error: error.message });
+    expect(result).toEqual({
+      error: 'Website doesnt found',
+    });
+  });
+
+  it('should return an error if an exception occurs', async () => {
+    const websiteId = '123';
+    const error = new Error('Some error');
+    Website.findById = jest.fn().mockRejectedValue(error);
+
+    const result = await startStopWebsitePart1(websiteId);
+
+    expect(Website.findById).toHaveBeenCalledWith(websiteId);
+    expect(result).toEqual({
+      error: 'Internal several error',
+    });
   });
 });
 describe('startStopWebsitePart2', () => {
